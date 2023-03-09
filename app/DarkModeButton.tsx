72bfcc7094f317cd98bce02f74283d410262bb04
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2023-03-09 10:43:54 -0800

    first commit

---
+++ app/DarkModeButton.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid'
+
+function DarkModeButton() {
+    const [mounted, setMounted] = useState(false);
+    const 
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DarkModeButton
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { categories } from "../constants";
+import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
+
+async function HomePage() {
+    // fetch the news data
+    const news: NewsResponse = await fetchNews(categories.join(","));
+    console.log(news)
+    
+    return (
+        <div>
+            <NewsList news={news} />
+        </div>
+    )
+}
+
+export default HomePage
